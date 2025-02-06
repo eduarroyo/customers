4e7d07ad0aadff1b4962c1d6352a26368ccd71fa
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2025-02-06 18:31:39 +0100

    Update customer endpoint

---
+++ src/Customers.Api/Customers/UpdateCustomer/UpdateCustomerCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using BuildingBlocks.CQRS;
+using Customers.Api.Data;
+using Customers.Api.Data.Entities;
+using Customers.Api.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microstore.Services.CatalogApi.Customers.UpdateCustomer;
+
+public record UpdateCustomerCommand
+(
+    Guid Id,
+    string Name,
+    string Surname,
+    EGender Gender,
+    DateTime BirthDate,
+    string Email,
+    string Address = default!,
+    string Country = default!,
+    string PostalCode = default!
+) : ICommand<UpdateCustomerResult>;
+
+public record UpdateCustomerResult(bool IsSuccess);
+
+internal class UpdateCustomerCommandHandler
+    (CustomersContext dbContext)
+    : ICommandHandler<UpdateCustomerCommand, UpdateCustomerResult>
+{
+    public async Task<UpdateCustomerResult> Handle(UpdateCustomerCommand command, CancellationToken cancellationToken)
+    {
+        Customer? customer = await dbContext.Customers.FirstOrDefaultAsync(c => c.Id == command.Id, cancellationToken);
+        if (customer is null)
+        {
+            throw new CustomerNotFoundException(command.Id);
+        }
+
+        customer.Name = command.Name;
+        customer.Surname = command.Surname;
+        customer.Gender = command.Gender;
+        customer.BirthDate = command.BirthDate;
+        customer.Email = command.Email;
+        customer.Address = command.Address;
+        customer.Country = command.Country;
+        customer.PostalCode = command.PostalCode;
+
+        dbContext.Customers.Update(customer);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateCustomerResult(true);
+    }
+}
---
+++ src/Customers.Api/Customers/UpdateCustomer/UpdateCustomerEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using Carter;
+using Customers.Api.Data.Entities;
+using Mapster;
+using MediatR;
+
+namespace Microstore.Services.CatalogApi.Customers.UpdateCustomer;
+
+public record UpdateCustomerRequest
+(
+    Guid Id,
+    string Name,
+    string Surname,
+    EGender Gender,
+    DateTime BirthDate,
+    string Email,
+    string Address = default!,
+    string Country = default!,
+    string PostalCode = default!
+);
+
+public record UpdateCustomerResponse(bool IsSuccess);
+
+public class UpdateCustomerEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/customers", async (UpdateCustomerRequest request, ISender sender) =>
+        {
+            UpdateCustomerCommand command = request.Adapt<UpdateCustomerCommand>();
+            UpdateCustomerResult result = await sender.Send(command);
+            UpdateCustomerResponse response = result.Adapt<UpdateCustomerResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("UpdateCustomer")
+        .Produces<UpdateCustomerResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Customer")
+        .WithDescription("Update Customer");
+    }
+}
